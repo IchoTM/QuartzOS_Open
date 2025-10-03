a6c3e963337052acc97e96f1166beb1213b304b9
IchoTM <IchoTM@users.noreply.github.com>
2025-10-02 21:06:21 -0400

    Refactor Cargo.toml, main.rs, and add configuration files for x86_64 target

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@
 
 use::core::panic::PanicInfo;
 
-#[unsafe(no_mangle)] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
-
 //Panic Handling
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop{}
+}
+
+#[unsafe(no_mangle)] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    loop {}
 }

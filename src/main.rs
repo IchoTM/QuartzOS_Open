e525f127ddf37e7a3a39637433bbb6b155a6fae4
IchoTM <IchoTM@users.noreply.github.com>
2025-10-14 12:24:10 -0400

    Begins the roots of testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
-#![feature(asm)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use::core::panic::PanicInfo;
 
@@ -13,11 +15,19 @@ fn panic(info: &PanicInfo) -> ! {
     loop{}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Intentional Crash");
+
+    #[cfg(test)]
+    test_main();
     loop {}
 }
